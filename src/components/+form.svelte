2c3ca40bd35244296325301e1415cbf507e351fa
Anissoft <anissoft@yandex-team.com>
2022-12-07 17:06:43 +0700

    initial commit

---
+++ src/components/+form.svelte
@@ -0,0 +1,64 @@
+<script>
+
+</script>
+
+<form>
+  <p class="form-text">
+    <label for="source">Video from URL:</label> 
+    <input type="text" id="source" name="source" placeholder="YouTube link or direct link to video file"><br/>
+    <label for="second">Should play exact second:</label>
+    <input type="number" id="second" name="second" min="0" max="" step="1" value="0"><br/>
+    <label for="time">When current time is: </label>
+    <input type="time" id="time" name="time" step="10" value="00:00:00"><br/>
+  <p>
+</form> 
+
+<style>
+  input {
+    background-color: unset;
+    border: none;
+    font-size: 18px;
+    border: 1px var(--border-color) solid;
+    outline: none;
+    color: var(--font-color);
+  }
+
+  label {
+    display: inline-block;
+    min-width: 232px;
+  }
+
+  .form-text {
+    white-space: nowrap;
+    line-height: 25px;
+
+    font-size: 18px;
+  }
+
+  input#source {
+    width: calc(100% - 250px);
+  }
+
+  input#second {
+    width: 50px;
+  }
+
+  input#time {
+    width: 200px;
+  }
+
+  @media(max-width: 768px) {
+    .form-text {
+      white-space: normal;
+    }
+
+    label, input#source {
+      width: 100%;
+      max-width: 100%;
+    }
+
+    input {
+      margin-bottom: 16px;
+    }
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import Form from '../components/+form.svelte';
+</script>
+
+<div class="root">
+  <div class="container">
+    <Form />
+  </div>
+</div>
+
+<style>
+  .root {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    position: relative;
+
+    padding: 64px 32px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-sizing: border-box;
+  }
+  .container {
+    width: 100%;
+    height: 100%;
+    max-width: 1024px;
+
+    background-color: var(--surface-color);
+    border: 1px var(--border-color) solid;
+    border-radius: 10px;
+
+    padding: 32px;
+
+    box-sizing: border-box;
+    overflow-x: hidden;
+    overflow-y: scroll;
+  }
+
+  @media(max-width: 768px) {
+    .root {
+      padding: 0;
+    }
+
+    .container {
+      padding: 16px;
+
+      border: none;
+      border-radius: 0;
+    }
+  }
+</style>
